5682ece9b4194721c0437df9f31d620051c5f201
theo-githu <111968864+theo-githu@users.noreply.github.com>
2022-12-31 17:06:30 +0000

    Update main-view.jsx
    MovieView rendering

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,10 +8,7 @@ import { MovieView } from "../movie-view/movie-view";
 export const MainView = () => {
     const [selectedMovie, setSelectedMovie] = useState(null);
     const [movies, setMovies] = useState([]);
-    if (selectedMovie) {
-      return (<MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-      );
-    }
+
 
     useEffect(() => {
         fetch("https://myflix-api-1234.herokuapp.com/movies")
@@ -31,6 +28,11 @@ export const MainView = () => {
             setMovies(data)
           })
       }, []);
+
+    if (selectedMovie) {
+      return (<MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      );
+    }
 
     if (movies.length === 0) {
         return <div>The list is empty!</div>;
