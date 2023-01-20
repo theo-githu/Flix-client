6a24a7a883bfa3a38f5f8acd741bf7dd05edf785
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-01-20 21:00:56 +0000

    Update movie-card.jsx
    remove scss link

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,8 +4,6 @@ import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-// import './movie-card.scss';
-
 
 export const MovieCard = ({ movie }) => {
 
