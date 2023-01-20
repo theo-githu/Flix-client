206139fd89c36a6958d86eb31e57751d0af6e0b5
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-01-20 21:00:43 +0000

    Update movie-view.jsx
    Trying the way laid out in exercise

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+// import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 
@@ -10,27 +10,45 @@ export const MovieView = ({ movies }) => {
     const movie = movies.find((m) => m._id === movieId);
 
     return (
-        <Container>
-            <Card>
-                <Card.Body>
-                    <Card.Img variant="top" src={movie.ImageURL}/>
-                    <Card.Title className=" fw-bold mt-3">{movie.Title}</Card.Title>
-                    <Card.Text className="fw-light">{movie.Description}</Card.Text>
-                    <Card.Text className="fw-bolder mb-1">Genre: </Card.Text>
-                    <Card.Text className="fw-light mt-1">{movie.Genre.Name} </Card.Text>
-                    <Card.Text className="fw-bolder mb-1">Definition of Genre: </Card.Text>
-                    <Card.Text className="fw-light mt-1">{movie.Genre.Description}</Card.Text>
-                    <Card.Text className="fw-bolder mb-1">Director: </Card.Text>
-                    <Card.Text className="fw-light mt-1">{movie.Director.Name}</Card.Text>
-                    <Card.Text className="fw-bolder mb-1">IMDb Rating: </Card.Text>
-                    <Card.Text className="fw-light mt-1">{movie.Rating}</Card.Text>
-                    <Link to={`/`}>
-                        <Button className="back-button" variant="primary" size="sm">Back</Button>
-                    </Link>
-                </Card.Body>
-            </Card>
-        </Container>
+        <div>
+            <div>
+                <img className="w-100" src={movie.ImageURL} />
+            </div>
+            <div>
+                <span>Title:</span>
+                <span>{movie.Title}</span>
+            </div>
+            <div>
+                <span>{movie.Description}</span>
+            </div>
+            <Link>
+                <button className="back-button">Back</button>
+            </Link>
+        </div>
     );
+
+    // return (
+    //     <Container>
+    //         <Card>
+    //             <Card.Body>
+    //                 <Card.Img variant="top" src={movie.ImageURL}/>
+    //                 <Card.Title className=" fw-bold mt-3">{movie.Title}</Card.Title>
+    //                 <Card.Text className="fw-light">{movie.Description}</Card.Text>
+    //                 <Card.Text className="fw-bolder mb-1">Genre: </Card.Text>
+    //                 <Card.Text className="fw-light mt-1">{movie.Genre.Name} </Card.Text>
+    //                 <Card.Text className="fw-bolder mb-1">Definition of Genre: </Card.Text>
+    //                 <Card.Text className="fw-light mt-1">{movie.Genre.Description}</Card.Text>
+    //                 <Card.Text className="fw-bolder mb-1">Director: </Card.Text>
+    //                 <Card.Text className="fw-light mt-1">{movie.Director.Name}</Card.Text>
+    //                 <Card.Text className="fw-bolder mb-1">IMDb Rating: </Card.Text>
+    //                 <Card.Text className="fw-light mt-1">{movie.Rating}</Card.Text>
+    //                 <Link to={`/`}>
+    //                     <Button className="back-button" variant="primary" size="sm">Back</Button>
+    //                 </Link>
+    //             </Card.Body>
+    //         </Card>
+    //     </Container>
+    // );
  };
  
             
