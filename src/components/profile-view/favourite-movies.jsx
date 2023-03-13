b42ba39858675d0601779741c98b32cdc0e5c008
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-03-13 19:11:04 +0000

    Update favourite-movies.jsx
    Alter arrangement in coding

--- src/components/profile-view/favourite-movies.jsx
+++ src/components/profile-view/favourite-movies.jsx
@@ -1,35 +1,39 @@
 import React, {useEffect, useState} from "react";
-import { Col, Button, Card } from "react-bootstrap";
+import { Row, Col, Button, Card } from "react-bootstrap";
 
 import { MovieCard } from "../movie-card/movie-card";
 
-export const FavouriteMovies = ({ storedUser, movies }) => {
-    const [user, setUser] = useState(storedUser ? storedUser : null);
+export const FavouriteMovies = ({ user, favMovies }) => {
     
-    let favouriteMovieList = movies.filter((m) =>
-    user.FavouriteMovies.includes(m.id)    
-    );
-    
+    const favouriteMovies = favMovies.map((movie) => {
+        return {
+            id: movie._id,
+            title: movie.Title,
+            image: movie.ImageURL,
+            description: movie.Description,
+            genre: movie.Genre,
+            director: movie.Director
+        };
+    });
+
     return (
         <Row>
-            {favouriteMovieList.length === 0 ? (
+            {favouriteMovies.length === 0 ? (
                 <Col>You don't have any favourite movies!</Col>
             ) : (
                 <>
-                <div className= 'text-start h3 mb-3'>Your Favourite Movies</div>
-                {/* {favouriteMovieList.map((movie) => (
-                    <Col className='mb-3' key={movie.id}
-                    <MovieCard>
-                    movieData={movie}
-                    user={user}
-                    updateUser={(user) => {
-                        console.log('Update user', user);
-                        setUser(user);
-                        localStorage.setItem('user', JSON.stringify(user));
-                    }}
-                    </MovieCard>
-                    </Col>
-                ))} */}
+                <Card className="bg-light mt-3">
+                    <Card.Title className="fw-bold fs-2">Your Favourite Movies</Card.Title>
+                    <Card.Body>
+                        <Row>
+                            {favouriteMovies.map((movie) => (
+                                <Col className="mb-3" key={movie.id} md={3}>
+                                    <MovieCard movie={movie} user={user} />
+                                </Col>
+                            ))}
+                        </Row>
+                    </Card.Body>
+                </Card>
                 </>
             )}
         </Row>
