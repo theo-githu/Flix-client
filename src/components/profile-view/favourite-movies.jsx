687a4f06c5e942ef4c2e346a6f11ddccc3405874
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-03-13 13:30:02 +0000

    Update favourite-movies.jsx

--- src/components/profile-view/favourite-movies.jsx
+++ src/components/profile-view/favourite-movies.jsx
@@ -1,25 +1,36 @@
-
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { Col, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-function FavouriteMovies ({ favouriteMovieList }) {
-    return (
-        <div>
-            <h2>Your Favourite Movies</h2>
-            {favouriteMovieList.map((movies) => {
-                return (
-                    <div key={movies._id}>
-                        <img src={movies.ImageURL} />
-                        <Link to={`/movies/${movies._id}`}>
-                            <h4>{movies.Title}</h4>
-                        </Link>
-                        <button variant="secondary" onClick={() => removeFavourite(movies._id)}>Remove from favourites</button>
-                    </div>
-                )
-            })
-            }
-        </div>
-    )
+import { ProfileView } from "../profile-view/profile-view";
+import { MovieCard } from "../movie-card/movie-card";
+
+export const FavouriteMovies = ({user, movies}) => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+    const storedMovie = JSON.parse(localStorage.getItem("movies"));
+
+    const [token] = useState(storedToken ? storedToken : null);
+    
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState("");
+    const [favouriteMovies, setFavouriteMovies] = useState([]);
+
+    const getUser = (token) => {
+        fetch(`https://myflix-api-1234.herokuapp.com/users/${user.Username}`, {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}`},
+    }).then(response => response.json())
+    .then((response) => {
+        console.log("getUser response", response)
+        setUsername(response.Username)
+        setEmail(response.Email)
+        setPassword(response.Password)
+        setBirthday(response.Birthday);
+        setFavouriteMovies(response.FavouriteMovies)
+    })
+}
 }
 
-export default FavouriteMovies
