02eb12b4815f37afb0f13a8269916f3f72b485c9
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-03-13 13:18:21 +0000

    Update update-user.jsx
    create update user page

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,38 +1,107 @@
 
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
 
-function UpdatedUser ({ handleSubmit, handleUpdate }) {
+export const UpdateForm = ({ user }) => {
+    const storedToken = localStorage.getItem("token");
+    const storedMovie = JSON.parse(localStorage.getItem("movies"));
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+
+    const [token] = useState(storedToken ? storedToken : null);
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState("");
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+
+        const data = {
+            Username: username, 
+            Password: password, 
+            Email: email,
+            Birthday: birthday
+        };
+        console.log(data)
+
+        const updateUser = await fetch(`https://myflix-api-1234.herokuapp.com/users/${user.Username}`, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+        })
+
+        const response = await updateUser.json()
+        console.log(response)
+        if (response) {
+            alert("User information successfully updated! Please log in.");
+            localStorage.clear();
+            window.location.reload();
+        } else {
+            alert("Something went wrong! Please try again.");
+        }
+    };
+
+    const handleDeregister = () => {
+        fetch(`https://myflix-api-1234.herokuapp.com/users/${user.Username}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer $(token)`,
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            if (response.ok) {
+                alert("Account successfully deleted! Sorry to see you go.");
+                localStorage.clear();
+                window.location.reload();
+            } else {
+                alert("Something went wrong! Please try again.");
+            }
+        });
+    }
+
     return (
-        <form className="profile-form" onSubmit={(e) => handleSubmit(e)}>
-            <h2>Update your information</h2>
-            <label>Username:</label>
-            <input 
-                type="text"
-                name="Username"
-                defaultValue={user.Username}
-                onChange={e => handleUpdate(e)}
-            />
-
-            <label>Password:</label>
-            <input 
-                type="password"
-                name="Password"
-                defaultValue={user.Password}
-                onChange={e => handleUpdate(e)}
-            />
-
-            <label>Email:</label>
-            <input 
-                type="email"
-                name="Email"
-                defaultValue={user.Email}
-                onChange={e => handleUpdate(e.target.value)}
-            />
-            <button variant="primary" type="submit">
-                Update 
-            </button>
-        </form>
+        <>
+        <h4>Update User Profile</h4>
+        <Form onSubmit={(e) => handleSubmit(e)}>
+            <Form.Group>
+                <Form.Label>Username:</Form.Label>
+                <Form.Control>
+                    type="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Password:</Form.Label>
+                <Form.Control>
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Email:</Form.Label>
+                <Form.Control>
+                    type="text"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control>
+                    type="date"
+                    value={birthday}
+                    onChange={e => setBirthday(e.target.value)}
+                </Form.Control>
+            </Form.Group>
+            <Button type="submit" variant="primary">Save Changees</Button>
+        </Form>
+        <Button onClick={() => handleDeregister(user._id)} className="button-delete mt-3" type="submit" variant="danger">Delete Account</Button>
+        </>
     )
 }
-
-export default UpdateUser
